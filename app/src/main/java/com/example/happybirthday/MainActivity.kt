90aa36022923c9d5b0e1b29423b2e8fc887dc3e5
FerZacVal <ferzac21@gmail.com>
2025-08-29 04:24:31 -0500

    Actividad 5 - Ejercicio 1 Articulo Compose

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,9 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,16 +25,18 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingImage(
-                        stringResource(R.string.happy_birthday_text),
-                        stringResource(R.string.signature_text)
+                ){
+                    GreetingContent(
+                        top = stringResource(R.string.caja_superior),
+                        mid = stringResource(R.string.caja_medio),
+                        bot = stringResource(R.string.caja_fondo),
+                        card = painterResource(R.drawable.bg_compose_background)
                     )
                 }
             }
@@ -45,58 +45,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    // Create a column so that texts don't overlap
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
+fun GreetingContent(top: String, mid: String, bot: String, card: Painter, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(painter = card, contentDescription = null)
         Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 16.dp)
+            text = top,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
         )
         Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .padding(end = 16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-
+            text = mid,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = bot,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
         )
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    // Create a box to overlap image and texts
-    Box(modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.androidparty),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
-    }
-}
-
-@Preview(showBackground = false)
-@Composable
-private fun BirthdayCardPreview() {
+fun Act5Exer1Preview() {
     HappyBirthdayTheme {
-        GreetingImage(
-            stringResource(R.string.happy_birthday_text),
-            stringResource(R.string.signature_text)
+        GreetingContent(
+            top = stringResource(R.string.caja_superior),
+            mid = stringResource(R.string.caja_medio),
+            bot = stringResource(R.string.caja_fondo),
+            card = painterResource(R.drawable.bg_compose_background)
         )
     }
 }
